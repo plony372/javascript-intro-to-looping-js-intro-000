e0c09bd9a2a07587da320801b0f889a0c2c1e75e
plony benav <menachemfuterfas@gmail.com>
2021-02-18 21:17:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -7,7 +7,7 @@ let i = 0;
 
 function forLoop(array){
   for (let num = 0; num < 25; num++){
-    console.log(`"I am ${num} strange loop${num === 0 ? ' ' : 's'}."`)
+    console.log(`"I am ${num} strange loop${num === 0 ? ' ' : 's'}. to an array 25 times"`)
     array.push(num)
     }
   return array
